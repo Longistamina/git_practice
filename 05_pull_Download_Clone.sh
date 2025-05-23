e9c26db933a4bb21192ee2b39407f268ed05cea3
Longistamina <dophamthelong03@outlook.com>
2025-05-23 12:15:20 +0700

    modifed pull

--- 05_pull_Download_Clone.sh
+++ 05_pull_Download_Clone.sh
@@ -6,3 +6,21 @@ git pull origin #A combination of fetch and merge.
                 #It is used to pull all changes from a remote repository into the branch you are working on.
 
 git pull origin --allow-unrelated-histories #To allow pull even if local and remote have unrelated histories
+
+#-----------------#
+#-----------------#
+
+# If your local branch and the remote branch have diverged 
+# (meaning both have new commits that the other doesn’t have) 
+# Git wants you to decide how to merge those changes.
+
+git pull --no-rebase origin 
+#Git will create a merge commit combining both histories.
+#Should use this
+
+git pull --rebase origin 
+#Git will replay your local commits on top of the remote branch, creating a linear history.
+
+git pull --ff-only origin 
+#Git will only update if your local branch is behind the remote (no divergence). 
+#If there's a divergence, it will refuse to pull.
