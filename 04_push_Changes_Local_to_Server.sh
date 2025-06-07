89f13cefd35dcecaec0a7538e7cc69fc633e08e0
Longistamina <dophamthelong03@gmail.com>
2025-06-07 18:42:04 +0700

    modified push and pull

--- 04_push_Changes_Local_to_Server.sh
+++ 04_push_Changes_Local_to_Server.sh
@@ -19,11 +19,15 @@ git branch -M main #To set "main" as default branch, not "master"
 # On the right side, choose HTTPS and copy that URL_link
 # for example: https://codeberg.org/Longistamina/git_practice.git
 
-git remote set-url origin https://codeberg.org/your-username/your-repo.git #use this if want to replace github with codeberg
+# Or copy the shh ones (RECOMMENDED for security)
+# for example: ssh://git@codeberg.org/Longistamina/git_practice.git
 
-git remote add origin URL_link #add the remote repository to local repository
+git remote add origin ssh://git@codeberg.org/Longistamina/git_practice.git #add the remote repository to local repository
+
+git remote set-url origin ssh://git@codeberg.org/Longistamina/git_practice.git #use this if want to replace github with codeberg
 
 git push -u origin main
+
 
 
 ### Check if codeberg has been set as origin
@@ -32,6 +36,14 @@ git remote -v
 #=> origin https://codeberg.org/Longistamina/git_practice.git (fetch)
 #=> origin https://codeberg.org/Longistamina/git_practice.git (push)
 
-### If use codeberg as a second platform beside github (origin)
-git remote add codeberg https://codeberg.org/your-username/your-repo.git
-git push -u codeberg main
+
+
+### If use github as a second platform codeberg github (origin)
+git remote add github git@github.com:User_Name/Repository_Name.git
+git push -u github main
+
+
+######## if you prioritize the history of the LOCAL
+######## In other words, you want to replace all the history of the remote with the local
+git push -u --force origin main
+git push -u --force github main
--- 05_pull_Changes_Server_to_Local.sh
+++ 05_pull_Changes_Server_to_Local.sh
@@ -5,14 +5,20 @@
 # origin master: the branch "master" of the remote origin
 # origin other_branch: the branch "other_branch" of the remote origin
 
+# origin here is codeberg
+
 git fetch origin #fetch gets all the change history of a tracked branch/repo.
+git fetch github
 
 git merge origin/main #merge the "main" branch of remote origin to local
+git merge github/main
 
 git pull origin main #A combination of fetch and merge. 
                      #Pull everything from the "main" branch of remote origin to local
+git pull github main
 
 git pull origin --allow-unrelated-histories main #To allow pull even if local and remote have unrelated histories
+git pull github --allow-unrelated-histories main
 
 #-----------------#
 #-----------------#
@@ -21,13 +27,38 @@ git pull origin --allow-unrelated-histories main #To allow pull even if local an
 # (meaning both have new commits that the other doesn’t have) 
 # Git wants you to decide how to merge those changes.
 
-git pull --no-rebase origin main #Can be other branch different from "main"
+git pull --no-rebase origin main
+git pull --no-rebase github main
+#Can be other branch different from "main"
 #Git will create a merge commit combining both histories.
+#Your local commits and the remote commits coexist, and the merge commit records the integration point.
+#Your local history remains intact; commits are not reordered or rewritten.
 #Should use this
 
-git pull --rebase origin main #Can be other branch different from "main"
+git pull --rebase origin main 
+git pull --rebase github main 
+#Can be other branch different from "main"
 #Git will replay your local commits on top of the remote branch, creating a linear history.
+#Clean
 
-git pull --ff-only origin main #Can be other branch different from "main"
+git pull --ff-only origin main
+git pull --ff-only github main 
+#Can be other branch different from "main"
 #Git will only update if your local branch is behind the remote (no divergence). 
 #If there's a divergence, it will refuse to pull.
+
+
+######## if you prioritize the history of the REMOTE
+######## In other words, you want to replace all the history of the local with the remote
+
+# Fetch the latest changes from the remote
+git fetch origin
+git fetch github
+
+# Then, reset your local branch to the remote branch, discarding local commits and changes
+git reset --hard origin/main
+git reset --hard github/main
+
+# Now can safely pull
+git pull origin main
+git pull github main
