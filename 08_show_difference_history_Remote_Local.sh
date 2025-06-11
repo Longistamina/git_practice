a250358aceef2763a787ca146f10e09d8f6f625b
Longistamina <dophamthelong03@gmail.com>
2025-06-12 00:28:41 +0700

    added show_difference_history

---
+++ 08_show_difference_history_Remote_Local.sh
@@ -0,0 +1,32 @@
+# origin here is codeberg
+
+# 1) Fetch the latest remote updates (to ensure your remote tracking branches are up to date):
+git fetch origin main # or other branch_name
+git fetch github main # or other branch_name
+
+
+# 2) View the commits that are in your local branch but not in the remote branch (i.e., the "ahead" commits):
+git log origin/main..main # or other branch_name
+git log github/main..main # or other branch_name
+# If output shows nothing => no commits ahead
+
+
+# 3) Alternatively, to see the actual code differences between your local branch and the remote branch:
+git diff origin/main..main # or other branch_name
+git diff github/main..main # or other branch_name
+# If output shows nothing => no commits difference
+
+
+# 4) To see how many commits your branch is ahead or behind the remote branch
+git rev-list --left-right --count origin/main...main # or other branch_name
+git rev-list --left-right --count github/main...main # or other branch_name
+# The ... (triple-dot) notation in git rev-list calculates the symmetric difference between the two branches, 
+# showing commits reachable from either branch but not from both
+# Output: 0 0
+# ------> indicates no commits exist exclusively on either side; the branches have identical commit histories
+
+
+# 5) Use show to see the local and remote history in detailed
+git show main          # show the history of LOCAL "main" branch (can be other branch_name)
+git show origin/main   # show the history of REMOTE origin (codeberg) "main" branch (can be other branch_name)
+git show github/main   # show the history of REMOTE github            "main" branch (can be other branch_name)
